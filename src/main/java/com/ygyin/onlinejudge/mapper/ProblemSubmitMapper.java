bda797a39d5fa40ee2557693fbb060f59830dd04
YGYin <ygyin0307@gmail.com>
2024-05-08 20:54:01 +0800

    Problem submit mapper updated

---
+++ src/main/java/com/ygyin/onlinejudge/mapper/ProblemSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.onlinejudge.mapper;
+
+import com.ygyin.onlinejudge.model.entity.ProblemSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【problem_submit(题目提交)】的数据库操作Mapper
+* @createDate 2024-03-30 18:15:12
+* @Entity com.ygyin.onlinejudge.model.entity.ProblemSubmit
+*/
+public interface ProblemSubmitMapper extends BaseMapper<ProblemSubmit> {
+
+}
+
+
+
+
