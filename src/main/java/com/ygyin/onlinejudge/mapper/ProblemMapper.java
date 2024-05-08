3ef1c8d73011b0ac11bcf8ecd86ec4a2cd28fa84
YGYin <ygyin0307@gmail.com>
2024-05-08 20:53:44 +0800

    Problem mapper updated

---
+++ src/main/java/com/ygyin/onlinejudge/mapper/ProblemMapper.java
@@ -0,0 +1,18 @@
+package com.ygyin.onlinejudge.mapper;
+
+import com.ygyin.onlinejudge.model.entity.Problem;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yg
+* @description 针对表【problem(题目)】的数据库操作Mapper
+* @createDate 2024-03-30 18:11:42
+* @Entity com.ygyin.onlinejudge.model.entity.Problem
+*/
+public interface ProblemMapper extends BaseMapper<Problem> {
+
+}
+
+
+
+
